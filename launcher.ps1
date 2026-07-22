eeede90562d99a24eb65be2058418f5f80a59a30
egzakutacno <egzakutacno@gmail.com>
2026-07-22 13:56:21 +0800

    fix launcher: use scriptblock::create instead of | iex -Retry

--- launcher.ps1
+++ launcher.ps1
@@ -1,7 +1,7 @@
 param([switch]$Retry)
 
 if (![Security.Principal.WindowsPrincipal]::new([Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(527) -and !$Retry) {
-    Start-Process powershell -Verb RunAs -ArgumentList "-NoP -c `"[Net.ServicePointManager]::SecurityProtocol='Tls12'; irm https://raw.githubusercontent.com/egzakutacno/daljinac-pack/main/launcher.ps1 | iex -Retry`""
+    Start-Process powershell -Verb RunAs -ArgumentList "-NoP -c `$c=irm https://raw.githubusercontent.com/egzakutacno/daljinac-pack/main/launcher.ps1; &([scriptblock]::create(`$c)) -Retry"
 } else {
     irm https://raw.githubusercontent.com/egzakutacno/daljinac-pack/main/install.ps1 | iex
 }
